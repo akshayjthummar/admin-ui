ee154612b938b0b4062471c89876033d286d9686
akshayjthummar <akshaythummar34@gmail.com>
2024-09-05 13:19:57 +0530

    add attribute

--- src/pages/products/form/Attributes.tsx
+++ src/pages/products/form/Attributes.tsx
@@ -1,7 +1,62 @@
-import React from "react";
+import { Card, Col, Form, Radio, Row, Switch, Typography } from "antd";
+import { Category } from "../../../types";
 
-const Attributes = () => {
-  return <div>Attributes</div>;
+type AttributeProps = {
+  selectedCategory: string;
 };
-
+const Attributes = ({ selectedCategory }: AttributeProps) => {
+  const category: Category | null = selectedCategory
+    ? JSON.parse(selectedCategory)
+    : null;
+  if (!category) return null;
+  return (
+    <Card
+      title={<Typography.Text>Product Attributes</Typography.Text>}
+      bordered={false}
+    >
+      {category.attributes.map((attribute) => {
+        return (
+          <div key={attribute.name}>
+            {attribute.widgetType === "radio" ? (
+              <Form.Item
+                label={attribute.name}
+                name={["attributes", attribute.name]}
+                initialValue={attribute.defaultValue}
+                rules={[
+                  { required: true, message: `${attribute.name} is required` },
+                ]}
+              >
+                <Radio.Group>
+                  {attribute.availableOptions.map((option) => {
+                    return (
+                      <Radio.Button value={option} key={option}>
+                        {option}
+                      </Radio.Button>
+                    );
+                  })}
+                </Radio.Group>
+              </Form.Item>
+            ) : attribute.widgetType === "switch" ? (
+              <Row>
+                <Col>
+                  <Form.Item
+                    label={attribute.name}
+                    name={["attributes", attribute.name]}
+                    valuePropName="checked"
+                    initialValue={attribute.defaultValue}
+                  >
+                    <Switch
+                      checkedChildren="Yes"
+                      unCheckedChildren="No"
+                    ></Switch>
+                  </Form.Item>
+                </Col>
+              </Row>
+            ) : null}
+          </div>
+        );
+      })}
+    </Card>
+  );
+};
 export default Attributes;
